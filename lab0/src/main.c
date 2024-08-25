d43ce5a12e569601b066703e5e8c26b907efb988
Ariel0425 <143051839+Ariel0425@users.noreply.github.com>
2024-08-25 10:57:09 -0400

    lab0 step4

---
+++ lab0/src/main.c
@@ -0,0 +1,15 @@
+#include "stm32f0xx.h"
+
+int main(void) {
+    int x = 0;
+    int y = 0;
+
+    while (1) {
+        for (x = 0; x < 10; x++) {
+
+        }
+        y++;
+        x = 0;
+    }
+    return 0;
+}
